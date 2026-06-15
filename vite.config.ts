85ce216756ca032091428d8b4e0af8972c7e1f0c
AHMEDMOUSTAPHA17 <analys7.a.m.falah@gmail.com>
2026-06-15 11:54:25 +0200

    Change base path in Vite config to './'

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: "CONTRA",
+    base: "./",
     server: {
       port: 5173,
       host: "0.0.0.0",
